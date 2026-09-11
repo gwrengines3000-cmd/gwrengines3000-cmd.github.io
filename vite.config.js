292af448ba97e1f4455fda0dfa32f5551237a6fa
Stephen Ramirez <800101411@fvtc.edu>
2026-09-11 16:49:32 -0500

    added base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/https://github.com/gwrengines3000-cmd/gwrengines3000.github.io.git/', // Replace with your desired base path
 })
